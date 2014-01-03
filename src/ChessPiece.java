96d93dd443087f1fc1f1eb2ad6ba96fdd8a093df
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2014-01-02 19:05:56 -0500

    Generated basic classes, resources folder.

---
+++ src/ChessPiece.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ChessPiece{
+	
+	private ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<ArrayList<Integer>>();
+	public ChessPiece(){
+		ArrayList<Integer> positionCoordinates = new ArrayList<Integer>(2);
+	}
+	public static void main(String[] args){
+	}
+}
