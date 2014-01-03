8f78586289382c00d4d9df3f23fa8cad3f2740ad
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2014-01-02 19:17:02 -0500

    Minor edits.

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 public class ChessPiece{
-	
-	private ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<ArrayList<Integer>>();
-	public ChessPiece(){
-		ArrayList<Integer> positionCoordinates = new ArrayList<Integer>(2);
+	private int value, identity;
+	private ArrayList<ArrayList<Integer>> moves = new ArrayList<ArrayList<Integer>>();
+	public ChessPiece(int pieceValue, int pieceIdentity, ArrayList<ArrayList<Integer>> possibleMoves){
+		value = pieceValue;
+		identity = pieceIdentity;
+		moves = possibleMoves;
 	}
+	
 	public static void main(String[] args){
 	}
 }
